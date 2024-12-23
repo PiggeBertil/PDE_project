3f84837759625a34af2d0951232bcad6acf6c432
Savinjith Walisadeera <savwal@chalmers.se>
2024-12-23 16:08:48 +0100

    davids fungerande kod

--- BIE/BIE_Projectq4.m
+++ BIE/BIE_Projectq4.m
@@ -12,29 +12,8 @@ nu2 = nu2 ./ sqrt( rvec.^2+ rprimvec.^2 );
 
 A_k = zeros(N,N);
 vecdsdt = sqrt(rprimvec.^2+rvec.^2);
-p = [0,4];
-nu = [nu1; nu2];
-k=1;
-for i = 1:N
-    for j = 1:N
-        nu_i = [nu1(i), nu2(i)];
-    
-        r_j = [y1(j), y2(j)];
-        r_i = [y1(i), y2(i)];
-        
-        diff = r_i - r_j;
-        hankel_diff = besselh(1,1,k*norm(diff));
-        auxillary = (1i*k/4)*hankel_diff/(norm(diff)); 
-    
-        A_k(i,j) = dot(nu_i, diff)*auxillary;
-    end
-end
-
-for i=1:N
-    taljare = rprimvec(i)^2-0.5*rbisvec(i)*rvec(i) + 0.5*rvec(i)^2;
-    namnare = 2*pi*(rprimvec(i)^2+rvec(i)^2)^(3/2);
-    A_k(i,i) = taljare/namnare;    
-end
+p = [0;4];
+%
 
 %%
 M = 300; % pick something here
@@ -49,20 +28,22 @@ hvec = ineverse_matrix\gvec.';
 hvec = hvec.';
 
 for ix1=1:M
-    for ix2=1:M
-        x1=x1field(ix1);
+   for ix2=1:M
+         x1=x1field(ix1);
         x2=x2field(ix2);
         t=angle(complex(x1,x2));
         radius= 3+cos(4*t+pi); %formula for rvec
         if x1^2+ x2^2< radius^2
-           phivec = -1i/4 * besselh(0,k*sqrt((x1-y1).^2 + (x2-(y2)).^2)); %kernel expression in terms of xi, yi, nui, i=1,2
+           phivec = (-1i / 4) * besselh(0, k * vecnorm([y1;y2] - [x1;x2])); %kernel expression in terms of xi, yi, nui, i=1,2
            exactfield(ix1,ix2) = real(1i/4*besselh(0,k*sqrt(x1.^2+(x2-4).^2)));
-           ufield(ix1,ix2) = (phivec*((hvec.* vecdsdt).')*2*pi/N)-4;
+           ufield(ix1,ix2) = (phivec*((hvec.* vecdsdt).')*2*pi/N);
         end
     end
 end
 
 ufield = real(ufield);
+errorfield = log10(abs(ufield-exactfield));
+
 %% 
 imagesc(x1field, x2field, exactfield.')
 axis xy
@@ -72,9 +53,7 @@ colorbar
 title('Analytical solution of u(x,y)')
 
 %%
-errorfield = log10(abs(ufield-exactfield));
 imagesc(x1field, x2field, ufield.')
-k=1;
 axis xy
 colormap turbo
 pbaspect([1 1 1])
@@ -83,7 +62,6 @@ title('Numerical solution of u(x,y)')
 
 %%
 imagesc(x1field, x2field, errorfield.')
-
 axis xy
 colormap turbo
 pbaspect([1 1 1])
--- BIE/david.m
+++ BIE/david.m
@@ -1,5 +1,5 @@
 %% Problem 1
-N= 1000; % number of dicretization points on the curve
+N= 500; % number of dicretization points on the curve
 tvec= linspace(-pi + 2*pi/N, pi, N);
 
 rvec= 3+cos(4.* tvec + pi); % analytic expression for r(t)
@@ -20,7 +20,7 @@ nu2= nu2 ./ sqrt( rvec.^2+ rprimvec.^2 );
 k = 1;
 omega = k;
 A = zeros(N);
-vecdsdt = sqrt(rprimvec.^2+rvec.^2);
+dsdtvec = sqrt(rprimvec.^2+rvec.^2);
 for i = 1:N
     for j = 1:N
         nu_i = [nu1(i), nu2(i)];
@@ -38,28 +38,49 @@ end
 
 %We need to fix the diagonals
 for i = 1:N
-    numerator = rprimvec(i)^2 - 0.5*rbisvec(i)*rvec(i)+0.5*rvec(i)^2;
-    denominator = 2*pi*(rprimvec(i)^2 + rvec(i)^2)^(3/2);
-    
-    A(i,i) = numerator/denominator;
+numerator = rprimvec(i)^2 - 0.5*rbisvec(i)*rvec(i)+0.5*rvec(i)^2;
+denominator = 2*pi*(rprimvec(i)^2 + rvec(i)^2)^(3/2);
+dsdt = dsdtvec(i);
+A(i,i) = numerator/denominator;
 end
+
+subplot(1,3,1)
+imagesc(tvec,tvec,real(A).')
+axis xy
+colorbar
+title('re kmat')
+
+subplot(1,3,2)
+imagesc(tvec,tvec,imag(A).')
+axis xy
+colorbar
+title('im kmat')
+
+subplot(1,3,3)
+imagesc(tvec,tvec,abs(A).')
+axis xy
+colorbar
+title('abs kmat')
+
+%% Problem 2
 
 p = [0; -3];
-uAn = @(x,y) -(1i/4)*besselh(0,k*norm([x;y]-p));
-dx_u = @(x,y) 1i*k/(4*norm([x;y]-p)) * besselh(1,k*norm([x;y]-p))*(x-p(1));
-dy_u = @(x,y) 1i*k/(4*norm([x;y]-p)) * besselh(1,k*norm([x;y]-p))*(x-p(2));
+uAn = @(x,y) -(1i/4)*besselh(0,1,k*norm([x;y]-p));
 
-g1 = zeros(1,N);
-g2 = zeros(1,N);
-for i = 1:N
-    g1(i) = dx_u(y1(i), y2(i))*nu1(i);
-    g2(i) = dy_u(y1(i), y2(i))*nu2(i);
-end
-gvec = g1'+g2';
+%{
+dx_u = @(x,y) 1i*k./(4*vecnorm([x;y]-p)) .* besselh(1,1,k*vecnorm([x;y]-p)).*(x-p(1));
+dy_u = @(x,y) 1i*k./(4*vecnorm([x;y]-p)) .* besselh(1,1,k*vecnorm([x;y]-p)).*(y-p(2));
 
-kmat = (-eye(N)/2+ 2*pi/N* A *diag(vecdsdt));
+g1 = dx_u(y1,y2).*nu1;
+g2 = dy_u(y1,y2).*nu2;
 
-hvec = kmat \ gvec;
+gvec = g1.'+g2.';
+%}
+
+gvec = 1i*k/4 * besselh(1,1,k*vecnorm([y1;y2]-p))./vecnorm([y1;y2]-p) .* (dot(([y1;y2]-p),[nu1;nu2]));
+kmat = (-eye(N)/2+ 2*pi/N* A *diag(dsdtvec));
+
+hvec = kmat \ gvec.';
 hvec = hvec.';
 
 M= 300; % pick something here
@@ -70,27 +91,77 @@ uAnField = zeros(M,M);
 
 
 for ix1=1:M
-    for ix2=1:M
-        x1=x1field(ix1);
-        x2=x2field(ix2);
-        t=angle(complex(x1,x2));
-        radius= 3 + cos(4 * t + pi); %formula for rvec
-        if x1^2+ x2^2 < radius^2
-            phivec= (-1i / 4) * besselh(0, k * vecnorm([y1;y2] - [x1;x2])); %kernel expression in terms of xi, yi, nui, i=1,2
-            vfield(ix1,ix2) = (phivec *(hvec.* vecdsdt).')*2*pi/N; %trapezoidal rule
-            uAnField(ix1,ix2) = real(uAn(x1,x2));
-        end
+for ix2=1:M
+    x1=x1field(ix1);
+    x2=x2field(ix2);
+    t=angle(complex(x1,x2));
+    radius= 3 + cos(4 * t + pi); %formula for rvec
+    if x1^2+ x2^2 < radius^2
+        phivec= (-1i / 4) * besselh(0,1, k * vecnorm([y1;y2] - [x1;x2])); %kernel expression in terms of xi, yi, nui, i=1,2
+        vfield(ix1,ix2) = (phivec *(hvec.* dsdtvec).')*2*pi/N; %trapezoidal rule
+        uAnField(ix1,ix2) = uAn(x1,x2);
     end
 end
-ufield = real(vfield);
+end
 
-%% 
-imagesc(x1field, x2field, ufield.')
+t = 0;
+ufield_real = real(vfield*exp(-1i*omega*t));
+ufield_im = imag(vfield*exp(-1i*omega*t));
+
+uAnField_real = real(uAnField);
+uAnField_im = imag(uAnField);
+
+subplot(2,3,1)
+imagesc(x1field, x2field, ufield_real.')
 axis xy
 colormap turbo
 pbaspect([1 1 1])
 colorbar
-title('Analytical solution of u(x,y)')
+title('Numerical real solution')
+
+subplot(2,3,4)
+imagesc(x1field, x2field, ufield_im.')
+axis xy
+colormap turbo
+pbaspect([1 1 1])
+colorbar
+title('Numerical im solution')
+
+subplot(2,3,2)
+imagesc(x1field, x2field, uAnField_real.')
+axis xy
+colormap turbo
+pbaspect([1 1 1])
+colorbar
+title('Analytic solution real')
 
 
+subplot(2,3,5)
+imagesc(x1field, x2field, uAnField_im.')
+axis xy
+colormap turbo
+pbaspect([1 1 1])
+colorbar
+title('Analytic solution im')
+%% Problem 2 part 2
+%Find the average in a square inside
+average_numerical_real = mean(mean(ufield_real(M/3:2*M/3, M/3:2*M/3)));
+average_analytic_real = mean(mean(uAnField_real(M/3:2*M/3, M/3:2*M/3)));
 
+average_numerical_im = mean(mean(ufield_im(M/3:2*M/3, M/3:2*M/3)));
+average_analytic_im = mean(mean(uAnField_im(M/3:2*M/3, M/3:2*M/3)));
+
+subplot(2,3,3)
+imagesc(x1field, x2field, log10(abs(ufield_real-average_numerical_real+average_analytic_real-uAnField_real)).')
+axis xy
+colormap turbo
+pbaspect([1 1 1])
+colorbar
+title('Log10 error')
+
+subplot(2,3,6)
+imagesc(x1field, x2field, log10(abs(ufield_im-average_numerical_im+average_analytic_im-uAnField_im)).')
+axis xy
+colormap turbo
+pbaspect([1 1 1])
+colorbar
