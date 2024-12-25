9098894ede0de31d7262a235599a2157352fab8e
EliasGuedra <90322377+EliasGuedra@users.noreply.github.com>
2024-12-25 11:35:31 +0100

    working

--- FEM/main.m
+++ FEM/main.m
@@ -8,9 +8,17 @@ B = zeros(N, N);
 for element = 1 : size(t, 2)
     %Calculate A_nn and B_nn for adjecent node.
     nn = t(1:3, element);
-    A(nn, nn) = A(nn, nn) + IntMatrix( p(:, nn) );
+    A(nn, nn) = A(nn,nn) + IntMatrix( p(:, nn) );
     B(nn, nn) = B(nn, nn) + IntMatrix2( p(:, nn) );
 end
+
+
+nn= size(p, 2);
+intnodes= setdiff(1:nn, e(1, :)); % list of non-boundary nodes 
+ADir= A(intnodes, intnodes); % removes boundary rows/columns
+A=ADir;
+BDir= B(intnodes , intnodes);
+B=BDir;
 
 
 %Minimize det(A-lamda*B) to recieve our eigenvalues approximations
@@ -28,8 +36,22 @@ smallest_eigenvalues = sorted_eigenvalues(1:50);
 
 % Display results
 disp('The 50 smallest eigenvalues are:');
-disp(smallest_eigenvalues);
-
-%Extract f from the coordinates. u = f_1*phi_1 + ... + f_n*phi_n
+%plot(smallest_eigenvalues)
 
 %plot u for all f:s
+%pdeplot(p,e,t,XYData=eigenvectors(:,10))
+
+%% b)
+
+analytic_eigenvalues = [];
+
+for n = 1:50
+    for m = 1:50
+        analytic_eigenvalues = [analytic_eigenvalues; pi^2*(n^2/4 + m^2)];
+    end
+end
+
+analytic_eigenvalues = sort(analytic_eigenvalues);
+
+
+
